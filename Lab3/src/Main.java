be90a95531255851e0bc2742c540b78255c30cb0
916-Serban-Cristian <72075997+916-Serban-Cristian@users.noreply.github.com>
2022-10-26 15:42:25 +0300

    Add files via upload

---
+++ Lab3/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            var programReader = new java.util.Scanner(new File("resources/p1.ciba"));
+            var program = new StringBuilder();
+            while (programReader.hasNextLine()) {
+                program.append(programReader.nextLine()).append('\n');
+            }
+            programReader.close();
+            var tokensReader = new java.util.Scanner(new File("resources/token.in"));
+            var tokens = new ArrayList<String>();
+            while (tokensReader.hasNextLine()) {
+                tokens.add(tokensReader.nextLine());
+            }
+            var scanner = new Scanner(program.toString(), tokens);
+            try {
+                scanner.scan();
+            } catch (ScannerException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+            scanner.outST();
+            scanner.outPIF();
+            System.out.println("lexically correct");
+        } catch (java.io.FileNotFoundException e) {
+            System.out.println("Source file not found");
+        } catch (IOException e) {
+            System.out.println("Can't write output");
+        }
+    }
+}
---
+++ Lab3/src/SymbolTable.java
@@ -0,0 +1,56 @@
+import java.util.Hashtable;
+
+/**
+ * This class implements a symbol table for the compiler. It is a wrapper around
+ * the Java Hashtable class. It stores the tokens as values and their hash codes
+ * as keys.
+ *
+ * @param <T> The type of the tokens.
+ */
+public class SymbolTable<T> {
+    private final Hashtable<Integer, T> table;
+
+    public SymbolTable() {
+        table = new Hashtable<>();
+    }
+
+    /**
+     * Add a new token to the symbol table.
+     *
+     * @param symbol The token to add.
+     */
+    public void add(T symbol) {
+        table.put(symbol.hashCode(), symbol);
+    }
+
+    /**
+     * Get token at a given position.
+     *
+     * @param position The position of the symbol
+     * @return The symbol at the position
+     */
+    public T get(Integer position) {
+        return table.get(position);
+    }
+
+    /**
+     * Looks for a token in the symbol table and returns its position.
+     * If the token is not found, it is added to the table and its position is returned.
+     *
+     * @param symbol The token to look for.
+     * @return The position of the token in the symbol table.
+     */
+    public Integer pos(T symbol) {
+        if (!table.contains(symbol.hashCode())) {
+            add(symbol);
+        }
+        return symbol.hashCode();
+    }
+
+    @Override
+    public String toString() {
+       var str = new StringBuilder();
+       table.forEach((key, value) -> str.append(key).append(" -> ").append(value).append('\n'));
+       return str.toString();
+    }
+}
